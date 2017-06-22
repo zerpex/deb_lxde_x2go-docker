f6df4aec2ad03d6e2872390fdbc99f8b14509102
zerpex <zerpex@gmail.com>
2017-06-22 09:11:05 +0200

    [enh] add user & pass as var

--- first_run.sh
+++ first_run.sh
@@ -4,6 +4,9 @@
 TZ="${TIME_ZONE:-Europe/Paris}"
 CP="${TIME_ZONE:-UTF-8}" 
 LANG="${LANGUAGE:-fr_FR}" 
+USR="${USER:-logan}"
+UPASS="${USER_PASS:-$(pwgen -s 12 1)}"
+PASS="${ROOT_PASS:-$(pwgen -s 12 1)}"
 
 if [ -f /.first_run ]; then
         exit 0
@@ -16,29 +19,27 @@ echo "$TZ" > /etc/timezone && \
 dpkg-reconfigure --frontend noninteractive tzdata
 
 
-PASS="${ROOT_PASS:-$(pwgen -s 12 1)}"
 _word=$( [ ${ROOT_PASS} ] && echo "preset" || echo "random" )
 echo "=> Setting a $_word password to the root user"
 echo "root:$PASS" | chpasswd
 
-adduser --disabled-password --gecos "" dockerx
-adduser dockerx sudo
-DPASS=$(pwgen -s 12 1)
+adduser --disabled-password --gecos "" $USR
+adduser $USR sudo
 
 echo "=> Setting a password to the docker user"
-echo "dockerx:$DPASS" | chpasswd
+echo "$USR:$UPASS" | chpasswd
 
 
 echo "=> Done!"
 touch /.first_run
 
-echo "========================================================================"
+echo "================================================================="
 echo "You can now connect to this container via SSH using:"
 echo ""
 echo "    ssh -p <port> root@<host>"
 echo "and enter the root password '$PASS' when prompted"
 echo ""
-echo " dockerx password : $DPASS "
+echo " $USR password : $UPASS "
 echo "use this to connect to the x2go server from your x2go client!"
 echo "Please remember to change the above password as soon as possible!"
-echo "========================================================================"
+echo "================================================================="
